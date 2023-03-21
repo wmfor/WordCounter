bdc6f6d187fd84a8edff1115b0e1e2e66ac6f051
Weston Forbes <westonmichaelforbes@gmail.com>
2023-03-21 12:16:22 -0400

    C# Version Finished

---
+++ C#/WordCounter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WordCounter 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int characterCount = 0;
+            int wordCount = 0;
+
+            string s = "";
+            
+            Console.WriteLine("Paste the paragraph/text you'd like to count.");
+            
+            s = Console.ReadLine();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0 && s[0] != ' ')
+                    wordCount = 1;
+                
+                characterCount++;
+                if (s[i] == ' ')
+                {
+                    wordCount++;
+                }
+            }
+            
+            Console.WriteLine("\n Character Count is - " + characterCount + " / " + "Word Count is - " + wordCount);
+        }
+    }
+}
